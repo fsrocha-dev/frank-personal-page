d04c5774b7c7d3bc965c349af1e6732c4c88f4df
fsrocha-dev <contato.frankrocha@gmail.com>
2021-07-31 18:30:18 -0300

    Cria component Header

---
+++ components/Header/index.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Frank Rocha</h1>
+    </header>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
         <meta name="description" content="Venha aprender e se divertir com javascript!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main className={styles.main}>
       </main>
 
