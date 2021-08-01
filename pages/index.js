d3ef6ab77bef2e4f44ed9e1adb438b1c75770c62
fsrocha-dev <contato.frankrocha@gmail.com>
2021-08-01 14:22:29 -0300

    :sparkles: Cria função para manipular localStorage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import Light from '../styles/themes/light'
@@ -7,10 +7,11 @@ import Dark from '../styles/themes/dark'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import useThemeSwitcher from '../hook/useThemeSwitcher.js'
 
 
 export default function Home(props) {
-  const [theme, setTheme] = useState(Dark)
+  const [theme, setTheme] = useThemeSwitcher(Dark)
 
   const changeTheme = () => {
     setTheme(theme.title === 'dark' ? Light : Dark)
@@ -32,7 +33,11 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const githubData = await fetch(`https://api.github.com/users/${process.env.userName.github}`).then(res => res.json())
+  // const githubData = await fetch(`https://api.github.com/users/${process.env.userName.github}`).then(res => res.json())
+  const githubData = {
+    avatar_url: "https://avatars.githubusercontent.com/u/38115122?v=4",
+    name: 'Frank Rocha'
+  }
 
   const URL = process.env.appEnv === 'local' ? process.env.urlDev : process.env.urlProd
 
