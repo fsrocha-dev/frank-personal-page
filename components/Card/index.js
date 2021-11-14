e3b0eb486012446bc221f5040a47d91d8617dd15
fsrocha-dev <contato.frankrocha@gmail.com>
2021-11-14 15:08:12 -0300

    Clean branch

--- components/Card/index.js
+++
@@ -1,9 +0,0 @@
-import { CardLink } from './styles'
-export default function Card({ title, description, link }) {
-  return (
-    <CardLink href={link} target="_blank" rel="noreferrer" alt={description}>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </CardLink>
-  )
-}
