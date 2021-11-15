b87cd5a04b6251b23611d7a1c51e9c4ff930c874
fsrocha-dev <contato.frankrocha@gmail.com>
2021-11-15 15:17:31 -0300

    Add active style to navbar

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,8 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 export default function Header() {
+  const router = useRouter()
   const pages = ['About', 'Articles']
 
   return (
@@ -11,16 +14,21 @@ export default function Header() {
           </button>
         </Link>
         <ul>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/articles">
-              <a>Articles</a>
-            </Link>
-          </li>
+          {pages.map(page => {
+            const path = `/${page.toLowerCase()}`
+            return (
+              <li
+                key={page}
+                className={
+                  router.pathname == path ? 'site-navbar-link__active' : ''
+                }
+              >
+                <Link href={path}>
+                  <a>{page}</a>
+                </Link>
+              </li>
+            )
+          })}
         </ul>
         <button type="button" aria-label="Command" className="btn-nav btn-cmd">
           <i className="ri-command-line"></i>
