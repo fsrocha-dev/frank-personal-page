c65c621da9dab5e5f9e7cd57163dbb301b84406d
fsrocha-dev <contato.frankrocha@gmail.com>
2021-11-15 11:43:14 -0300

    Add site navbar

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,29 @@
+import Link from 'next/link'
 export default function Header() {
   const pages = ['About', 'Articles']
 
   return (
-    <header>
-      Sou o header
-      <p>sdffsf</p>
+    <header className="site-header">
+      <nav className="site-navbar">
+        <Link href="/">
+          <a className="">FR</a>
+        </Link>
+        <div className="">
+          <ul>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/articles">
+                <a>Articles</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <button type="button" aria-label="Command" className=""></button>
+      </nav>
     </header>
   )
 }
