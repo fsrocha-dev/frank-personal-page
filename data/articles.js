c273a7fea33809801558bf29c1553304384d5ed0
fsrocha-dev <contato.frankrocha@gmail.com>
2021-11-19 14:35:06 -0300

    Fix image property inside article data

--- data/articles.js
+++ data/articles.js
@@ -4,56 +4,56 @@ const talks = [
     url: 'https://frankrochajs.medium.com/optional-chaining-745c03917a41',
     date: '2021-09-19',
     readingTime: 2,
-    imagePath: '/static/img/optional-chaining-2021-09-19.png'
+    imageName: 'optional-chaining-2021-09-19.png'
   },
   {
     title: 'Ciclo de vida do NPM',
     url: 'https://frankrochajs.medium.com/ciclo-de-vida-do-npm-10478511f5d1',
     date: '2021-08-11',
     readingTime: 2,
-    imagePath: '/static/img/ciclo-de-vida-npm-2021-08-11.png'
+    imageName: 'ciclo-de-vida-npm-2021-08-11.png'
   },
   {
     title: 'Optional Chaining',
     url: 'https://workos.com',
     date: '2021-09-19',
     readingTime: 1,
-    imagePath: '/static/img/optional-chaining-2021-09-19.png'
+    imageName: 'optional-chaining-2021-09-19.png'
   },
   {
     title: 'Mascarando uma String com JS',
     url: 'https://frankrochajs.medium.com/mascarando-uma-string-com-javascript-ee94d6a9e769',
     date: '2021-07-11',
     readingTime: 1,
-    imagePath: '/static/img/mascarando-string-js-2021-07-11.png'
+    imageName: 'mascarando-string-js-2021-07-11.png'
   },
   {
     title: 'Combinando Operadores',
     url: 'https://frankrochajs.medium.com/combinando-operadores-d61cdbc1b200',
     date: '2021-07-27',
     readingTime: 1,
-    imagePath: '/static/img/combinando-operadores-2021-07-27.png'
+    imageName: 'combinando-operadores-2021-07-27.png'
   },
   {
     title: 'Instalando Sass de forma rápida',
     url: 'https://frankrochajs.medium.com/instalando-sass-de-forma-r%C3%A1pida-847dc2d25d77',
     date: '2018-07-18',
     readingTime: 10,
-    imagePath: '/static/img/instalando-sass-2018-07-18.jpeg'
+    imageName: 'instalando-sass-2018-07-18.jpeg'
   },
   {
     title: 'Emmet para React no Sublime 3',
     url: 'https://frankrochajs.medium.com/emmet-para-react-no-sublime-3-a8a9016cd811',
     date: '2018-11-13',
     readingTime: 1,
-    imagePath: '/static/img/emmet-sublime-2018-11-13.png'
+    imageName: 'emmet-sublime-2018-11-13.png'
   },
   {
     title: 'Emmet para React no VS Code',
     url: 'https://frankrochajs.medium.com/emmet-para-react-no-vs-code-cb98fc2c639a',
     date: '2018-07-12',
     readingTime: 2,
-    imagePath: '/static/img/emmet-react-no-vscode-2018-07-12.jpeg'
+    imageName: 'emmet-react-no-vscode-2018-07-12.jpeg'
   }
 ]
 
