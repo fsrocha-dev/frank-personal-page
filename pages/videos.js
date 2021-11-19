22f99afb6c52193df45e8bd59ebc7fc13137f5dd
fsrocha-dev <contato.frankrocha@gmail.com>
2021-11-18 23:54:10 -0300

    Create video page content

---
+++ pages/videos.js
@@ -0,0 +1,78 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Main from '../layouts/Main'
+import videos from '../data/videos'
+
+function Videos(props) {
+  const { title, description } = props
+
+  return (
+    <div className="container green">
+      <Head>
+        <title>{title}</title>
+        <meta content={title} property="og:title" />
+        <meta content={description} name="description" />
+        <meta content={description} property="og:description" />
+        <meta content="https://frankrocha.dev/videos/" property="og:url" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="my-thought-box">Yes I like to teach what I know</div>
+
+      <h2 className="last-videos-heading">Featured Videos</h2>
+      <div className="last-videos-container">
+        {videos.map((video, index) => renderVideoItem({ video, index }))}
+      </div>
+    </div>
+  )
+}
+
+const renderVideoItem = props => {
+  const { video, index } = props
+
+  return (
+    <a
+      key={index}
+      href={video.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="video-item"
+    >
+      <div className="video-thumb">
+        <Image
+          layout="fill"
+          loading="eager"
+          quality="75"
+          // width={250}
+          // height={150}
+          src={getYoutubeTumbnail(video.url)}
+          alt={video.title}
+        />
+      </div>
+      <div className="last-video-content">
+        <h3>{video.title}</h3>
+        <h4>{video.channel}</h4>
+        <span>{video.views} VIEWS</span>
+      </div>
+    </a>
+  )
+}
+
+const getYoutubeTumbnail = url => {
+  const getIdFromURL = url.substr(-11)
+  const urlYoutubeTambnail = `https://i.ytimg.com/vi/${getIdFromURL}/maxresdefault.jpg`
+  return urlYoutubeTambnail
+}
+
+export async function getStaticProps() {
+  const meta = {
+    title: 'Videos // Frank Rocha',
+    description: 'My latest videos for the web'
+  }
+
+  return { props: meta }
+}
+
+Videos.Layout = Main
+
+export default Videos
