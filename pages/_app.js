2b3f9cbe24b2c72e59e05df4a67e2948ccaee17b
fsrocha-dev <contato.frankrocha@gmail.com>
2021-11-21 09:45:12 -0300

    Add command bar globaly

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../public/static/css/main.css'
 import 'remixicon/fonts/remixicon.css'
 import { AnimatePresence } from 'framer-motion'
+import CommandBar from '../components/CommandBar'
 
 const Noop = ({ children }) => children
 
@@ -8,11 +9,13 @@ function MyApp({ Component, pageProps, router }) {
   const Layout = Component.Layout || Noop
 
   return (
-    <Layout>
-      <AnimatePresence exitBeforeEnter initial={true} key={router.route}>
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-    </Layout>
+    <CommandBar>
+      <Layout>
+        <AnimatePresence exitBeforeEnter initial={true} key={router.route}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </CommandBar>
   )
 }
 
