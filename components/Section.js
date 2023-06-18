406b35a7582e8707a9ab877300f59a9677d10564
Frank Rocha <contato.frankrocha@gmail.com>
2023-06-18 13:58:24 -0300

    Refactor section component

--- components/Section.js
+++ components/Section.js
@@ -1,15 +1,24 @@
 import { motion } from 'framer-motion'
 
-const Section = ({ className = '', children, delay = 0 }) => (
-  <motion.div
-    className={className}
-    initial={{ y: 10, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.8, delay }}
-    mb={6}
-  >
-    {children}
-  </motion.div>
-)
+const Section = ({ className = '', children, delay = 0, justify = 'space-between' }) => {
+	const sectionStyle = {
+		display: 'flex',
+		justifyContent: justify,
+		gap: '10px',
+		flexWrap: 'wrap',
+	}
 
+	return (
+		<motion.div
+			className={className}
+			style={sectionStyle}
+			initial={{ y: 10, opacity: 0 }}
+			animate={{ y: 0, opacity: 1 }}
+			transition={{ duration: 0.8, delay }}
+			mb={6}
+		>
+			{children}
+		</motion.div>
+	)
+}
 export default Section
