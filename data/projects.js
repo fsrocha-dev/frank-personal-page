8a63bae1e61f2a1591a18cd9244187a755bc2559
Frank Rocha <contato.frankrocha@gmail.com>
2023-06-18 13:57:51 -0300

    Add project data file

---
+++ data/projects.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    name: 'Slidefy',
+    url: 'https://github.com/fsrocha-dev/slidefy',
+    description: 'A slideshow focused on reading markdown files.',
+    iconName: 'ri-slideshow-2-line',
+  },
+  {
+    name: 'Pomodor',
+    url: 'https://github.com/fsrocha-dev/pomodor',
+    description: 'Node CLI to pomodoro timer.',
+    iconName: 'ri-time-line',
+  },
+  {
+    name: 'GCC',
+    url: 'https://github.com/fsrocha-dev/git-conventional-cli',
+    description: 'Commit conventional the easy way with cli.',
+    iconName: 'ri-terminal-box-line',
+  },
+  {
+    name: 'ZSH Node Theme',
+    url: 'https://github.com/fsrocha-dev/node-zsh-theme',
+    description: 'My custom Zsh Node theme.',
+    iconName: 'ri-palette-line',
+  },
+  {
+    name: 'Ulauncher Palenight theme',
+    url: 'https://github.com/fsrocha-dev/ulauncher-palenight-theme',
+    description: 'A theme for Ulauncher based on Palenight Theme Vscode.',
+    iconName: 'ri-palette-line',
+  },
+  {
+    name: 'Kahoot Song',
+    url: 'https://github.com/fsrocha-dev/kahoot-song',
+    description: 'Special Kahoot music in your terminal.',
+    iconName: 'ri-terminal-line',
+  },
+  {
+    name: 'Titafail Song',
+    url: 'https://github.com/fsrocha-dev/titafail',
+    description: 'Funny Titanic song with flute crashing.',
+    iconName: 'ri-terminal-line',
+  },
+]
+
+export default projects
